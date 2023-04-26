43f9a39390738c494db17d36f9b9c431875c7045
GianbaBotassi <gianba89@gmail.com>
2023-04-26 17:18:18 +0200

    Ended exercise, used math.random method

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
-let ciao = prompt("eccoci");
-console.log(ciao);
+// Dichiaro variabili
+
+let nome, cognome, favColor, randomNumber, pwdGen;
+
+// Chiedo il nome all'utente
+
+nome = prompt("Inserisci il tuo nome");
+
+// Chiedo il cognome all'utente
+
+cognome = prompt("Inserisci il tuo cognome");
+
+// Chiedo il colore preferito all'utente
+
+favColor = prompt("Inserisci il tuo colore preferito");
+
+// variabile numero random
+
+randomNumber = Math.floor(Math.random() * 100);
+
+// Variabile con classe generata
+
+pwdGen = nome + cognome + favColor + randomNumber;
+
+// Inserisci pwd generata nell'id in html
+
+document.getElementById("pwd").innerHTML = pwdGen
